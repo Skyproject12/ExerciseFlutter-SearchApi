526e4f9074d92162dd8f9ccb9327ae5e2b97420b
Kadek Sumendra <sumendrakadek108@gmail.com>
2020-02-13 11:36:09 +0700

    Search api

---
+++ lib/model.dart
@@ -0,0 +1,15 @@
+class Posts{ 
+  final int userId; 
+  final int id; 
+  final String title; 
+  final String body; 
+  Posts({this.userId, this.id, this.title, this.body}); 
+  factory Posts.fromJson(Map<String, dynamic> json){ 
+    return new Posts( 
+      userId: json["userId"], 
+      id: json["id"], 
+      title: json["title"], 
+      body: json["body"]
+    );
+  }
+}
